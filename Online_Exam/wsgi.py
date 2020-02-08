560bde84ac30dbae6a4e7ec7edcd1f9823d0dc9f
reSetRoot <wei860925@gmail.com>
2020-02-09 04:21:53 +0800

    Test for deploying from Heroku.

--- Online_Exam/wsgi.py
+++ Online_Exam/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling     # 新加入
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Online_Exam.settings")
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+application = Cling(get_wsgi_application())     # <- 修改
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --pythonpath Online_Exam Online_Exam.wsgi --log-file -
